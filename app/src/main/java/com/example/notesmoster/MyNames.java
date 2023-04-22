85508d2d3aa326359a0a2cc903b085e17834c525
cshek <iamcshekharkr@gmail.com>
2023-04-22 16:14:45 +0530

    This is a third commit

---
+++ app/src/main/java/com/example/notesmoster/MyNames.java
@@ -0,0 +1,30 @@
+package com.example.notesmoster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MyNames extends AppCompatActivity {
+
+    public static final int REQUEST_CODE_ADD_NOTE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_names);
+
+        ImageView imageAddNoteMain =findViewById(R.id.imageAddNoteMain);
+        imageAddNoteMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(
+                        new Intent(getApplication(),CreateNoteActivity.class),REQUEST_CODE_ADD_NOTE
+                );
+            }
+        });
+
+    }
+}
